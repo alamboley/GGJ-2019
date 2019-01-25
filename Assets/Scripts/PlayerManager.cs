dc3aef02d2d11af3dc013daf694c79efa7fe3295
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2019-01-25 23:58:19 +0100

    rotating triangle

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
+    public float speedCircleRotation;
+
     public Player[] players;
+
+     public Slider mainSlider;
+     float oldSliderValue;
 
     void Start()
     {
-        
     }
-    
-    void Update()
+
+    public void OnSliderChanged()
+    {
+        Debug.Log(mainSlider.value);
+
+        foreach (Player player in players)
+        {
+            if (mainSlider.value > oldSliderValue)
+                player.transform.RotateAround(Vector3.zero, Vector3.forward, speedCircleRotation);
+            else
+                player.transform.RotateAround(Vector3.zero, Vector3.back, speedCircleRotation);
+        }
+
+        oldSliderValue = mainSlider.value;
+    }
+
+    public void AddPlayer()
     {
         
     }
