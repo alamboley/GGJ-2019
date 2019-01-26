3f96f66b82d615aae571b873209acf99ee0feee6
Lefevre Thomas <gsynuh@gmail.com>
2019-01-26 23:02:16 +0100

    AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public enum AudioTrack
+{
+    ARP1,
+    ARP2,
+    BASS,
+    KICK,
+    HHAT
+}
+
+[Serializable]
+public class AudioSourceRef
+{
+    public AudioSource source;
+    public AudioTrack track;
+}
+
+public class AudioManager : MonoBehaviour
+{
+
+    public List<AudioSourceRef> sources = new List<AudioSourceRef>();
+
+    float bpm = 127f;
+    double beattime;
+    double startTime;
+    int beat = 0;
+
+    public UnityEvent OnBeat;
+
+    public void ToggleMute(AudioTrack track)
+    {
+        foreach(AudioSourceRef sr in sources)
+        {
+            if (sr.track == track)
+                sr.source.mute = !sr.source.mute;
+        }
+    }
+
+    private void Awake()
+    {
+       
+        beattime = 60.0 / (double)bpm;
+
+        PlayAll();
+
+        lastDSPTime = startTime - 4 * beattime;
+    }
+
+    double lastDSPTime;
+
+    private void Update()
+    {
+        if( AudioSettings.dspTime - lastDSPTime > beattime) {
+            lastDSPTime = AudioSettings.dspTime;
+
+            if (OnBeat != null)
+                OnBeat.Invoke();
+        }
+
+    }
+
+
+
+    void PlayAll()
+    {
+        startTime = AudioSettings.dspTime + 4 * beattime;
+        foreach(AudioSourceRef sr in sources)
+        {
+            sr.source.PlayScheduled(startTime);
+        }
+    }
+}
