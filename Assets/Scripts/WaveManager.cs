46ea1de78f268d23079cd7771ce17226c2d2b68b
DESKTOP-0K884LG\Akadream <lortet.guillaume@gmail.com>
2019-01-26 10:52:44 +0100

    Adding Wave managing

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    public int currentWave = 1;
+    public int monsterCount = 1;
+    public int waveDuration = 10;
+    public float ratioSpawn = 1.9f;
+
+    public GameObject monster;
+    public GameObject spawn;
+
+    private float time = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWave = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time - time > waveDuration)
+        {
+            currentWave++;
+            time = Time.time;
+            SpawnMonsters();
+        }
+    }
+
+    void SpawnMonsters()
+    {
+        Vector3 position = GetRandomPosition();
+        monster = Instantiate(monster, position, Quaternion.identity) as GameObject;
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        float x = Random.Range(spawn.transform.localPosition.x, spawn.transform.localScale.x);
+        float y = Random.Range(spawn.transform.localPosition.y, spawn.transform.localScale.y);
+
+        return new Vector3(x, y, 0);
     }
 }
