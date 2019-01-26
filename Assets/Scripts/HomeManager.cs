e4e767b309c494ab714b8cd0edd297133d0fc61b
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2019-01-26 11:43:38 +0100

    Home has life

---
+++ Assets/Scripts/HomeManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeManager : MonoBehaviour
+{
+    public int life = 3;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+
+        if (life-- == 0)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
