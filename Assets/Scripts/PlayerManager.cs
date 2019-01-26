93b10d84c35c130ee57d5ccb63f852b74d34eff8
DESKTOP-0K884LG\Akadream <lortet.guillaume@gmail.com>
2019-01-26 01:35:54 +0100

    Add four directions prefabs for player

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public float speedCircleRotation;
 
-    public Player[] players;
+    public List<Player> players;
+
+    public Transform playerDownDirection;
+    public Transform playerUpDirection;
+    public Transform playerLeftDirection;
+    public Transform playerRightDirection;
 
      public Slider mainSlider;
      float oldSliderValue;
@@ -16,21 +22,55 @@ public class PlayerManager : MonoBehaviour
 
     public void OnSliderChanged()
     {
-        Debug.Log(mainSlider.value);
-
         foreach (Player player in players)
         {
             if (mainSlider.value > oldSliderValue)
+            {
                 player.transform.RotateAround(Vector3.zero, Vector3.forward, speedCircleRotation);
+            }
             else
+            {
                 player.transform.RotateAround(Vector3.zero, Vector3.back, speedCircleRotation);
+            }
         }
 
         oldSliderValue = mainSlider.value;
     }
 
+    /// <summary>
+    /// J'ai essayé de vérifier le nombre de joueur et d'en instancier grâce à des prefabs
+    /// Mais ça ne fonctionne pas
+    /// </summary>
     public void AddPlayer()
     {
-        
+        if(players.Count < 4)
+        {
+            /*foreach(Player player in players)
+            {
+                Destroy(player);
+            }
+            players.Clear();*/
+
+            switch (players.Count)
+            {
+                case 1:
+                    Instantiate(playerUpDirection);
+                    Instantiate(playerDownDirection);
+                    break;
+                case 2:
+                    Instantiate(playerUpDirection);
+                    Instantiate(playerLeftDirection);
+                    Instantiate(playerRightDirection);
+                    break;
+                case 3:
+                    Instantiate(playerUpDirection);
+                    Instantiate(playerDownDirection);
+                    Instantiate(playerLeftDirection);
+                    Instantiate(playerRightDirection);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
