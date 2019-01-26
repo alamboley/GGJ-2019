bb5c95a81a5bcc11ffe1d1383fa1f1d27928460d
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2019-01-26 12:33:35 +0100

    spawning enemies correctly

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -7,14 +7,21 @@ public class WaveManager : MonoBehaviour
     public int waveDuration = 10;
     public float ratioSpawn = 1.9f;
 
-    public GameObject monster;
-    public GameObject spawn;
+    public Enemy EnemyPrefab;
+    public Transform RootSpawner;
 
-    private float time = 0;
-    
+    float time = 0;
+    float diagonal;
+
+
     void Start()
     {
         currentWave = 1;
+
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+        diagonal = Mathf.Sqrt((height * height) + (width * width)) / 2;
     }
     
     void Update()
@@ -29,27 +36,13 @@ public class WaveManager : MonoBehaviour
 
     void SpawnMonsters()
     {
-        Vector3 position = GetRandomPosition();
-        Instantiate(monster, position, Quaternion.identity);
+        Instantiate(EnemyPrefab, GetRandomPosition(), Quaternion.identity, RootSpawner);
     }
 
     Vector3 GetRandomPosition()
     {
-        Bounds bounds = OrthographicBounds();
         Vector2 randomAngle = Random.insideUnitCircle.normalized;
-        float diagonal = Mathf.Sqrt((bounds.extents.x * bounds.extents.x) + (bounds.extents.y * bounds.extents.y)) / 2;
 
-        Debug.Log(randomAngle * diagonal);
-
-        return randomAngle;
-        //return Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
-    }
-
-    static Bounds OrthographicBounds()
-    {
-        float screenAspect = (float)Screen.width / (float)Screen.height;
-        float cameraHeight = Camera.main.orthographicSize * 2;
-        
-        return new Bounds(Camera.main.transform.position, new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return randomAngle * diagonal;
     }
 }
