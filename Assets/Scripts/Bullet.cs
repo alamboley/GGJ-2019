3e2c6c0b0078ee9bccb93e741f9109e798de8ec5
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2019-01-26 11:25:54 +0100

    fix firing

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -15,6 +15,9 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.tag == "Player")
+            return;
+
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
