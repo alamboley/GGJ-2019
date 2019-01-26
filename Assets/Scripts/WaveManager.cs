e068f79d08f6b68791209daa89ed88b642c6e6e7
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2019-01-26 11:02:14 +0100

    small fix

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
@@ -34,7 +32,7 @@ public class WaveManager : MonoBehaviour
     void SpawnMonsters()
     {
         Vector3 position = GetRandomPosition();
-        monster = Instantiate(monster, position, Quaternion.identity) as GameObject;
+        Instantiate(monster, position, Quaternion.identity);
     }
 
     Vector3 GetRandomPosition()
