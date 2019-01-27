3de043c775e30367ccb6ad13749d1e5a2ac5ff9f
Lefevre Thomas <gsynuh@gmail.com>
2019-01-27 11:42:28 +0100

    Game Manager

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public AudioManager audioManager;
+    public WaveManager waveManager;
+    public PlayerManager playerManager;
+
+
+
+}
